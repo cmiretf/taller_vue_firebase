386e91dc4f4cf3edabe6209ec325daedf93232cc
Carlos Miret <cmiretf@gmail.com>
2023-07-05 16:58:57 +0200

    Added apiKey info

--- src/components/login/LoginCard.vue
+++ src/components/login/LoginCard.vue
@@ -4,11 +4,11 @@
     <form @submit.prevent="login">
       <div>
         <label for="username">Usuario:</label>
-        <input type="text" id="username" v-model="username" required>
+        <input type="text" id="username" v-model="username" required />
       </div>
       <div>
         <label for="password">Contraseña:</label>
-        <input type="password" id="password" v-model="password" required>
+        <input type="password" id="password" v-model="password" required />
       </div>
       <button @click="login()" type="submit">Iniciar sesión</button>
     </form>
@@ -16,20 +16,72 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      username: '',
-      password: ''
+      username: "",
+      password: "",
     };
   },
+  async beforeCreate() {
+    Auth.onAuthStateChanged(Auth.getAuth(), async (user) => {
+      if (user) {
+        try {
+          const authTime =
+            (await user.getIdTokenResult()).claims.auth_time * 1000;
+          const sessionDuration = 1000 * 60 * 60 * 12; // 12 hour session
+          if (Date.now() > authTime + sessionDuration)
+            await this.expireSession();
+          else {
+            const remaining = authTime + sessionDuration - Date.now();
+            const twoHours = 1000 * 60 * 60 * 2;
+            this.setSessionTimeOut(remaining > twoHours ? remaining : twoHours);
+
+            this.userUnsubscribe = await this.fetchUser();
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            if (!this.$store.getters.isDemoEnv) {
+              updateNotifications();
+              updatePlan();
+            }
+
+            if (
+              this.$route.name === "LoginView" ||
+              this.$route.fullPath === "/"
+            ) {
+              const finalRoute = this.$route.query.redirect || "/dashboard";
+              this.$router.replace(finalRoute);
+            }
+          }
+        } catch {
+          const message = this.$t("genericError");
+          this.setSnackbar({ position: "top", type: "error", message });
+
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          this.logout();
+        }
+      } else {
+        this.userUnsubscribe();
+        this.clearSessionTimeOut();
+        if (
+          this.$route.name !== "LoginView" &&
+          this.$route.name !== "SharedNotificationDetailView"
+        ) {
+          const currentPath = this.$route.fullPath;
+          const redirect = currentPath !== "/" ? currentPath : undefined;
+          this.$router.push({ name: "LoginView", query: { redirect } });
+        }
+      }
+      this.waitingAuthUser = false;
+    });
+  },
   methods: {
-    ...mapActions(['configureUser']),
+    ...mapActions(["configureUser"]),
     async login() {
-      await this.configureUser(this.username)
-      this.$router.push({ name: 'home' })
-    }
-  }
+      await this.configureUser(this.username);
+      this.$router.push({ name: "home" });
+    },
+  },
 };
 </script>
---
+++ src/firebase-exports.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries //
+import * as Firebase from "firebase/app";
+import * as Analytics from "firebase/analytics";
+import * as Auth from "firebase/auth";
+import * as Firestore from "firebase/firestore";
+import * as Storage from "firebase/storage";
+import * as Functions from "firebase/functions";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKC11J6gbGP7IB8p7641C9ck5_XswEPNY",
+  authDomain: "tallernuevo-897dc.firebaseapp.com",
+  projectId: "tallernuevo-897dc",
+  storageBucket: "tallernuevo-897dc.appspot.com",
+  messagingSenderId: "420126894687",
+  appId: "1:420126894687:web:fd1a84ee0c4e983c4e49ea",
+  measurementId: "G-JVYC2BLG0J",
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
+const Database = Firestore.getFirestore(App);
+// Initialize Firestore References
+const UsersCollection = Firestore.collection(Database, "users");
+
+export {
+  Firebase,
+  App,
+  Analytics,
+  Auth,
+  Firestore,
+  Database,
+  Storage,
+  Functions,
+  UsersCollection,
+};
